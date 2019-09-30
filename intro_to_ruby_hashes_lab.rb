83b4ff7482214c4426df7d3ea945db8540a7cc35
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-30 20:19:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,10 @@
 def base_hash
-  video_game ={ :video_game => {}} 
+  video_game ={ 
+    :video_game1 => {
+      
+    }
+    
+  } 
   
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
